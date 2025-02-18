6044dd014e1f3f99896f5ad5df3322f5172bef3e
ming <1877152267@qq.com>
2025-02-18 18:46:52 +0800

    初始化前端完成

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://www.ming-code.work" target="_blank">
+         XXXXXXXXXXXXX
+        </a>
+      </a-layout-footer>
+
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
+
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
+
+app.use(Antd);
+app.use(router)
+
+
+
